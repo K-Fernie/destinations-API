4d5ce5bbd3939719f4ad665d1a2b64c366ac50cd
Katy Fernelius <kkin001@gmail.com>
2022-07-22 09:04:58 -0500

    Put is working with postman

--- server.js
+++ server.js
@@ -7,33 +7,6 @@ import bodyParser from "body-parser";
 import { MongoClient, ObjectId } from "mongodb";
 import { filterDestinations } from "./helper.js";
 
-//INTERNAL DB MEMORY
-// const destinationsDB = {
-//   12345: {
-//     destination: "Eiffel Tower",
-//     location: "Paris",
-//     description: "",
-//     photo:
-//       "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-//   },
-//   123456: {
-//     destination: "Champs elysees",
-//     location: "France",
-//     description: "",
-//     photo:
-//       "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-//   },
-//   12345678: {
-//     destination: "Big Ben",
-//     location: "London",
-//     description: "",
-//     photo:
-//       "https://images.unsplash.com/photo-1529655683826-aba9b3e77383?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
-//   },
-// };
-
-//TODO- install express with npm install express --save
-//TODO- create the app variable
 const app = express();
 const PORT = process.env.PORT || 3000;
 const CONNECTION_STRING =
@@ -87,6 +60,28 @@ MongoClient.connect(CONNECTION_STRING)
         .deleteOne({ _id: ObjectId(req.params.id) })
         .then((result) => {
           res.json("Destination deleted");
+        })
+        .catch((error) => console.error(error));
+    });
+
+    app.put("/destinations", (req, res) => {
+      cardCollection
+        .findOneAndUpdate(
+          { _id: ObjectId(req.body._id) },
+          {
+            $set: {
+              destination: req.body.destination,
+              location: req.body.location,
+              description: req.body.description,
+              photo: req.body.photo,
+            },
+          },
+          {
+            upsert: false,
+          }
+        )
+        .then((result) => {
+          res.json("Success");
         })
         .catch((error) => console.error(error));
     });
